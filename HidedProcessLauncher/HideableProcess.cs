0b655f2ef7b8475d0c06b3071622a3049b56ffde
kabestrus <jorge_turrado@hotmail.es>
2018-03-01 09:08:54 +0100

    Add console support

--- HidedProcessLauncher/HideableProcess.cs
+++ HidedProcessLauncher/HideableProcess.cs
@@ -5,119 +5,121 @@ using System.Threading.Tasks;
 
 namespace HidedProcessLauncher
 {
+  /// <summary>
+  /// Custom class inhertited of Process
+  /// </summary>
+  class HideableProcess : Process
+  {
     /// <summary>
-    /// Custom class inhertited of Process
+    /// Enum of states
     /// </summary>
-    class HideableProcess : Process
+    enum States
     {
-        /// <summary>
-        /// Enum of states
-        /// </summary>
-        enum States
-        {
-            Visible,
-            Hided
-        }
-        /// <summary>
-        /// State of the window
-        /// </summary>
-        States m_State;
-        /// <summary>
-        /// MainWindowHandle
-        /// </summary>
-        IntPtr m_WindowHandle = IntPtr.Zero;
-
-        /// <summary>
-        /// Member that says if the process is hideable
-        /// </summary>
-        bool m_bHideable;
-
-        /// <summary>
-        /// Property that says if the process is hideable
-        /// </summary>
-        public bool IsHideable
-        {
-            get
-            {
-                return m_bHideable;
-            }
-        }
-
-        /// <summary>
-        /// Constructor of the class
-        /// </summary>
-        public HideableProcess()
-        {
-            m_State = States.Visible;
-        }
-      
-        /// <summary>
-        /// Togle the visibility of the process
-        /// </summary>
-        public void TogleState()
-        {
-            if (m_State == States.Visible)
-            {
-                WindowAPI.HideMainWindow(m_WindowHandle);
-                m_State = States.Hided;
-            }
-            else if (m_State == States.Hided)
-            {
-                WindowAPI.ShowMainWindow(m_WindowHandle);
-                m_State = States.Visible;
-            }
-        }
-        /// <summary>
-        /// Show the MainWindow
-        /// </summary>
-        public void Show()
-        {
-            if (m_State == States.Hided)
-            {
-                WindowAPI.ShowMainWindow(m_WindowHandle);
-                m_State = States.Visible;
-            }
-        }
-        /// <summary>
-        /// Hide the MainWindow
-        /// </summary>
-        public void Hide()
-        {
-            if (m_State == States.Visible)
-            {
-                WindowAPI.HideMainWindow(m_WindowHandle);
-                m_State = States.Hided;
-            }            
-        }
-
-        /// <summary>
-        /// Start the Process
-        /// </summary>
-        /// <returns></returns>
-        public new async Task Start()
-        {          
-            base.Start();
-            try
-            {
-                //Wait for procces start
-                Thread.Sleep(1500);
-
-                //Wait for procces InputIdle
-                if (base.WaitForInputIdle(10000))
-                {
-                    m_WindowHandle = MainWindowHandle;
-                    m_bHideable = true;
-                }
-                else
-                {
-                    m_bHideable = false;
-                }
-            }
-            catch
-            {
-                m_bHideable = false;
-            }
-        }        
-        
+      Visible,
+      Hided
     }
+    /// <summary>
+    /// State of the window
+    /// </summary>
+    States m_State;
+    /// <summary>
+    /// MainWindowHandle
+    /// </summary>
+    IntPtr m_WindowHandle = IntPtr.Zero;
+
+    /// <summary>
+    /// Member that says if the process is hideable
+    /// </summary>
+    bool m_bHideable;
+
+    /// <summary>
+    /// Property that says if the process is hideable
+    /// </summary>
+    public bool IsHideable
+    {
+      get
+      {
+        return m_bHideable;
+      }
+    }
+
+    /// <summary>
+    /// Constructor of the class
+    /// </summary>
+    public HideableProcess()
+    {
+      m_State = States.Visible;
+    }
+
+    /// <summary>
+    /// Togle the visibility of the process
+    /// </summary>
+    public void TogleState()
+    {
+      if (m_State == States.Visible)
+      {
+        WindowAPI.HideMainWindow(m_WindowHandle);
+        m_State = States.Hided;
+      }
+      else if (m_State == States.Hided)
+      {
+        WindowAPI.ShowMainWindow(m_WindowHandle);
+        m_State = States.Visible;
+      }
+    }
+    /// <summary>
+    /// Show the MainWindow
+    /// </summary>
+    public void Show()
+    {
+      if (m_State == States.Hided)
+      {
+        WindowAPI.ShowMainWindow(m_WindowHandle);
+        m_State = States.Visible;
+      }
+    }
+    /// <summary>
+    /// Hide the MainWindow
+    /// </summary>
+    public void Hide()
+    {
+      if (m_State == States.Visible)
+      {
+        WindowAPI.HideMainWindow(m_WindowHandle);
+        m_State = States.Hided;
+      }
+    }
+
+    /// <summary>
+    /// Start the Process
+    /// </summary>
+    /// <returns></returns>
+    public new async Task Start()
+    {
+
+      base.Start();
+      try
+      {
+        //Wait for procces start
+        Thread.Sleep(1500);
+
+        //Wait for procces InputIdle
+        if (base.WaitForInputIdle(10000))
+        {
+          m_WindowHandle = MainWindowHandle;
+          m_bHideable = true;
+        }
+        else
+        {
+          m_bHideable = false;
+        }
+      }
+      catch (Exception ex)
+      {
+        m_WindowHandle = MainWindowHandle;
+        m_bHideable = m_WindowHandle != IntPtr.Zero;
+      }
+    }
+
+  }
 }
--- HidedProcessLauncher/Program.cs
+++ HidedProcessLauncher/Program.cs
@@ -1,22 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Diagnostics;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace HidedProcessLauncher
 {
-    static class Program
+  static class Program
+  {
+    /// <summary>
+    /// Punto de entrada principal para la aplicación.
+    /// </summary>
+    [STAThread]
+    static void Main()
     {
-        /// <summary>
-        /// Punto de entrada principal para la aplicación.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
-        }
+      Application.EnableVisualStyles();
+      Application.SetCompatibleTextRenderingDefault(false);
+      Application.Run(new MainForm());
     }
+  }
 }
